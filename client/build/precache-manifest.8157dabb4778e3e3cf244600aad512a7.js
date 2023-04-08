aff9aafbe754f7cecc7bc86241eb5079833ca7a0
@AnubhavS7 <anubhav@smalsus.com>
2023-04-08 20:37:43 +0530

    Build

---
+++ client/build/precache-manifest.8157dabb4778e3e3cf244600aad512a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d78ee6d1660081a73465d4fca5639d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5897caa03c4aab8128ee",
+    "url": "/static/css/main.c9621245.chunk.css"
+  },
+  {
+    "revision": "8e9c7176a02f420fc1e2",
+    "url": "/static/js/2.fa7baf3a.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.fa7baf3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5897caa03c4aab8128ee",
+    "url": "/static/js/main.d4d9cdf1.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "f19e0d0a7d0cefe5441892437fc64c18",
+    "url": "/static/media/memoriesLogo.f19e0d0a.png"
+  },
+  {
+    "revision": "ba08c599be80ec3b3ce6ce815e4c2af2",
+    "url": "/static/media/memoriesText.ba08c599.png"
+  }
+]);
